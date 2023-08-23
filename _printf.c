67e27bd90ee3799a018ff669feebc81f44efbbeb
SolomonOnwueyikpe <solofamily2003@yahoo.com>
2023-08-23 14:32:36 -0700

    project on specifiers

--- _printf.c
+++ _printf.c
@@ -1,10 +1,11 @@
 #include "main.h"
+int _printf(const char *format, ...);
 
 /**
- * _printf - gives output according to a format
+ * _printf - Gives output according to a format
  * @format: A char string with zero or more directives
- *          conversion specifier for c,%,and s
- *          other characters are printed.
+ *          conversion specifier for c, %, s, d and i,
+ *          Other characters are printed.
  * Return: To return number of characters printed
  */
 
@@ -13,8 +14,10 @@ int _printf(const char *format, ...)
 	int _chara_print = 0;
 	int jul;
 	va_list list_ofargs;
+
 	if (format == NULL)
 		return (-1);
+
 	va_start(list_ofargs, format);
 	for (jul = 0; format && format[jul] != '\0'; jul++)
 	{
@@ -50,6 +53,52 @@ int _printf(const char *format, ...)
 					str_len++;
 				write(1, str, str_len);
 				_chara_print += str_len;
+			}
+			else if (format[jul] == 'd' || format[jul] == 'i')
+			{
+				int num = va_arg(list_ofargs, int);
+				int num_digits = 0;
+				int temp = num;
+				int i;
+				char *buffer;
+
+				if (num < 0)
+				{
+					write(1, "-", 1);
+					_chara_print++;
+					num = -num;
+					temp = -temp;
+				}
+
+
+				while (temp != 0)
+
+				{
+					temp /= 10;
+					num_digits++;
+				}
+
+
+				buffer = malloc(num_digits + 1);
+				if (buffer == NULL)
+				{
+					va_end(list_ofargs);
+					return (-1);
+				}
+
+				buffer[num_digits] = '\0';
+
+
+				for (i = num_digits - 1; i >= 0; i--)
+				{
+					buffer[i] = (num % 10) + '0';
+					num /= 10;
+				}
+
+				write(1, buffer, num_digits);
+				_chara_print += num_digits;
+
+				free(buffer);
 			}
 		}
 	}
